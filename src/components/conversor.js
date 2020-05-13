f8e6b00d26ef3d9a7f6075ba2e193e3285963cd1
Felipe Fontoura <felipefontouramec@gmail.com>
2020-05-13 11:29:03 +0200

    Created constructor(props)
    Developed constructs to collect properties

--- src/components/conversor.js
+++ src/components/conversor.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 
 
 export default class Conversor extends Component {
+
+    //Construtor com as propriedades, após isso, conseguimos acessar as propriedades que vem do App.js
+    constructor(props) {
+        super(props);
+
+    }
+
     render() { 
         return ( //Todos componentes deve retornar apenas uma div, e todos meus elementos deve estar dentro dessa div
             <div className="conversor"> {/*Componente conversor*/}
-                <h2>USD para BRL</h2>
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>{/*Por estar dentro de uma classe, colocar this*/}
                 <input type="text"></input>{/*Valor que vamos converter*/}
                 <input type="button" value="Converter"></input>
                 <h2>Valor convertido</h2>
