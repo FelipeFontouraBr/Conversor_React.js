24fc461ec1236cb1fc46012b656535f3303a8cf1
Felipe Fontoura <felipefontouramec@gmail.com>
2020-05-13 10:58:38 +0200

    Updated conversor.js
    Updated with information about Conversor component

---
+++ src/components/conversor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+export default class Conversor extends Component {
+    render() { 
+        return ( //Todos componentes deve retornar apenas uma div, e todos meus elementos deve estar dentro dessa div
+            <div className="conversor"> {/*Componente conversor*/}
+                <p>It's a component</p>
+
+            </div>
+        )
+    }
+}
