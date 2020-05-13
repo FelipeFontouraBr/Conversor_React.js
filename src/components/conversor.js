ffea418a9004f374abf1fe061b39c6543cc88d71
Felipe Fontoura <felipefontouramec@gmail.com>
2020-05-13 11:43:59 +0200

    Update commits

--- src/components/conversor.js
+++ src/components/conversor.js
@@ -19,7 +19,7 @@ export default class Conversor extends Component {
         return ( //Todos componentes deve retornar apenas uma div, e todos meus elementos deve estar dentro dessa div
             <div className="conversor"> {/*Componente conversor*/}
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>{/*Por estar dentro de uma classe, colocar this*/}
-                <input type="text" onChange={(event) => {this.setState({moedaA_valor:event.target.value})}}></input>{/*Valor que vamos converter / onChange receberá uma function e vai atualizar o estado sempre que o campo mudar*/}
+                <input type="text" onChange={(event) => {this.setState({moedaA_valor:event.target.value})}}></input>{/*Valor que vamos converter / onChange receberá uma function e vai atualizar o estado sempre que o campo mudar / Explicação: event.target = onChange irá disparar um evento, esse evento irá pegar quem é o target (nesse caso, será o ELEMENTO INPUT) e o valor que estiver dentro dele é o que nós queremos */}
                 <input type="button" value="Converter"></input>
                 <h2>Valor convertido</h2>
 
