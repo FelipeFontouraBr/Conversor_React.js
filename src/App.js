be4e13efae8fbde3cdb4454f36a982f5f66803f3
Felipe Fontoura <felipefontouramec@gmail.com>
2020-05-13 16:11:41 +0200

    Component Created (EUR, GBP)

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ class App extends Component {
   render() { //Aqui é onde aparece os elementos que será utilizado na tela
     return (
       <div className="App">
-        <Conversor //Aqui vamos usar o componente conversor
-        moedaA="USD"
-        moedaB="BRL"
-        ></Conversor>
-        <Conversor //Real para dolar
-        moedaA="BRL"
-        moedaB="USD"
-        ></Conversor>
+        {/*Aqui vamos usar o componente conversor*/}
+        <div className="linha">
+          <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="USD"></Conversor>{/*Real para dolar*/}
+        </div>
+        <div className="linha">
+          <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="EUR"></Conversor>{/*Real para dolar*/}
+        </div>
+        <div className="linha">
+          <Conversor moedaA="GBP" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="GBP"></Conversor>{/*Real para dolar*/}
+        </div>
       </div>
     );
   }
